4ba2c793aa50552f738bd336ed5373a5a65a0fbf
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-03-20 17:47:43 +0300

    Initial Commit

---
+++ App420/App420App.swift
@@ -0,0 +1,17 @@
+//
+//  App420App.swift
+//  App420
+//
+//  Created by IGOR on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct App420App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
